f77e32a7cffc813235637ed8adf22619664e1ff4
stteeel <samiLiebre@proton.me>
2025-04-24 06:11:26 -0400

    feat(pwa): add initial web manifest configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import VitePWA from "@vite-pwa/astro";
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +10,35 @@ export default defineConfig({
 		plugins: [tailwindcss()],
 	},
 
-	integrations: [react()],
+	integrations: [
+		react(),
+		VitePWA({
+			injectRegister: "auto",
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+			manifest: {
+				name: "Media Player",
+				short_name: "App",
+				description: "Mi aplicación en Astro con PWA",
+				theme_color: "#0f172a",
+				start_url: "/",
+				background_color: "#ffffff",
+				display_override: ["window-controls-overlay", "standalone"],
+				display: "standalone",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+				],
+			},
+		}),
+	],
 });

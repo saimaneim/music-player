f7c4321c45d53bbf3232e40cfbaa426f1aa2b508
stteeel <samiLiebre@proton.me>
2025-04-25 04:49:12 -0400

    feat(PlayerBar): implement global state synchronization

--- src/components/PlayerBar/PlayerBar.tsx
+++ src/components/PlayerBar/PlayerBar.tsx
@@ -1,39 +1,37 @@
+// Icons
 import ArrowLeftIcon from "@/assets/icons/ArrowLeft";
 import ArrowRightIcon from "@/assets/icons/ArrowRight";
 import PauseIcon from "@/components/PlayerBar/icons/Pause";
-// Icons
 import PlayIcon from "@/components/PlayerBar/icons/Play";
-
-import { useState } from "react";
+// store
+import usePlayerStore from "@/store/usePlayerStore";
 
 const PlayerBar = () => {
-	const [play, setPlay] = useState<boolean>(true);
-
-	const handleClick = () => {
-		setPlay(!play);
-	};
+	const { togglePlay, isPlaying, track } = usePlayerStore();
 
 	return (
 		<section className="fixed bottom-0 left-0 z-50 bg-clip-padding backdrop-filter px-5 py-3 grid grid-row-2 grid-col-1 gap-2 items-center backdrop-blur-xs bg-secundary/40 bg-opacity-10 w-screen rounded-t-xl">
 			<span className="grid grid-row-2 grid-col-2 space-x-2 justify-start items-end">
 				<img
 					className="row-span-2 rounded-lg"
-					src="https://placehold.co/50"
+					src={track.picture}
 					alt="songIcon"
+					height={50}
+					width={50}
 				/>
-				<p className="font-semibold">HUMBLE</p>
-				<p className="col-start-2">Kendrick Lamar</p>
+				<p className="font-semibold">{track.title}</p>
+				<p className="col-start-2">{track.artist}</p>
 			</span>
 			<div className="flex gap-3 place-self-end py-1">
 				<button type="button">
 					<ArrowLeftIcon />
 				</button>
 				<button
-					onClick={handleClick}
+					onClick={togglePlay}
 					type="button"
 					className="col-start-2 rounded-full size-10 flex justify-center items-center active:scale-95 transition-transform duration-150"
 				>
-					{play ? <PlayIcon /> : <PauseIcon className="mr-1.5" />}
+					{isPlaying ? <PlayIcon /> : <PauseIcon className="mr-1.5" />}
 				</button>
 				<button type="button">
 					<ArrowRightIcon />

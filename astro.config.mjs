96cad8a4675e56daf1301d2efe53f61aab41d9a3
stteeel <samiLiebre@proton.me>
2025-04-24 23:21:37 -0400

    chore(config): update astro.config.mjs with new AstroPWA() settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,39 +13,34 @@ export default defineConfig({
 	integrations: [
 		react(),
 		AstroPWA({
-			injectRegister: "auto",
 			registerType: "autoUpdate",
 			workbox: {
-				globDirectory: "dist",
 				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
 			},
 			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
 			manifest: {
+				orientation: "portrait",
+				scope: "/",
 				name: "Media Player",
 				short_name: "Media Player",
 				description: "Reproductor de Audio",
 				theme_color: "#0f172a",
 				start_url: "/",
+				id: "",
 				background_color: "#0f172a",
-				"display_override": [
-					"window-controls-overlay",
-					"standalone",
-					"minimal-ui",
-					"browser"
-				],
-				id: "/",
 				display: "standalone",
 				icons: [
 					{
 						src: "pwa-192x192.png",
 						sizes: "192x192",
 						type: "image/png",
+						purpose: "maskable",
 					},
 					{
 						src: "pwa-512x512.png",
 						sizes: "512x512",
 						type: "image/png",
-						purpose: "any",
+						purpose: "maskable",
 					},
 				],
 			},

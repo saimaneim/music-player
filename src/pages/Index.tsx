55f25db468d4f4d333cc5fa17ad06ab23b43a5b4
stteeel <samiLiebre@proton.me>
2025-04-26 02:10:17 -0400

    refactor: extract index.tsx content into separate component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,59 +1,12 @@
+import Albums from "@/components/Albums/Albums";
+import PermissionButton from "@/components/PermissionButon/PermissionButton";
 import PlayerBar from "@/components/PlayerBar/PlayerBar";
-import Albums from "@/components/albums/Albums";
 
 export default function Index() {
 	return (
-		<main className="p-2 grid grid-cols-2 grid-rows-2 justify-items-center items-center gap-2">
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Freestyle</p>
-			</span>
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Hip Hop</p>
-			</span>
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Vallenato</p>
-			</span>
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Pop</p>
-			</span>
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Rap</p>
-			</span>
-
-			<span className="text-center">
-				<img
-					src="https://placehold.co/50"
-					alt="album"
-					className="rounded-xl size-40"
-				/>
-				<p>Phonk</p>
-			</span>
+		<main>
 			<Albums />
+			<PermissionButton />
 			<PlayerBar />
 		</main>
 	);

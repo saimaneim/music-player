f9777e23d6767d3fbf2e36bbf94f096f4a308d98
stteeel <samiLiebre@proton.me>
2025-04-26 02:31:55 -0400

    feat(PlayerBar): enhance progress bar and update icons
    - Added dot indicator at the end of the progress bar for better UX
    - Replaced old custom icons with Heroicons library imports

--- src/components/PlayerBar/PlayerBar.tsx
+++ src/components/PlayerBar/PlayerBar.tsx
@@ -1,8 +1,11 @@
 // Icons
-import ArrowLeftIcon from "@/assets/icons/ArrowLeft";
-import ArrowRightIcon from "@/assets/icons/ArrowRight";
-import PauseIcon from "@/components/PlayerBar/icons/Pause";
-import PlayIcon from "@/components/PlayerBar/icons/Play";
+import {
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	PauseIcon,
+	PlayIcon,
+} from "@heroicons/react/24/outline";
+
 // store
 import usePlayerStore from "@/store/usePlayerStore";
 
@@ -38,8 +41,9 @@ const PlayerBar = () => {
 				</button>
 			</div>
 
-			<div className="bg-primary/30 rounded-2xl h-1 w-full col-span-2">
-				<div className="bg-primary rounded-2xl h-1 w-[50%]" />
+			<div className="bg-primary/30 rounded-2xl h-1 w-full col-span-2 flex items-center">
+				<div className="bg-primary rounded-2xl h-1 w-[10%]" />
+				<div className="bg-primary/80 rounded-full size-2" />
 			</div>
 		</section>
 	);
